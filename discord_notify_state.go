92997f0ee927f5be599657a84c9fb1b7c0e84e82
Distortions81 <carlotto81@gmail.com>
2026-01-04 11:15:32 -0700

    gate worker notifications

--- discord_notify_state.go
+++ discord_notify_state.go
@@ -3,8 +3,6 @@ package main
 import "time"
 
 // workerNotifyState tracks per-user, per-worker notification state.
-// It is persisted so we don't alert "offline" unless the worker has previously
-// been observed online (and vice versa).
 type workerNotifyState struct {
 	Online bool
 	Since  time.Time
